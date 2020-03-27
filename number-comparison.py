569685419f0f56e48af0984cd75d9a05770cd08a
Jared Heeringa <jheeringa@student.waubonsee.edu>
2020-03-26 23:33:51 -0500

    First commit. Asks user for 2 numbers and checks if num1 is greater than num2

---
+++ number-comparison.py
@@ -0,0 +1,9 @@
+# Jared Heeringa
+# CIS116 HW #2 - GitHub Problem
+# 3/23/2020
+
+num1 = int(input("Please enter the first number:"))
+num2 = int(input("Please enter the second number:"))
+
+if num1 > num2:
+    print("num1 is greater than num2") 
